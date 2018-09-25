1384306c347fdbffda6090e587ab1d1a9db551fb
JANGHYUNn <kimjh2369@gmail.com>
2018-09-25 23:33:21 +0900

    배열

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
     render() {
         return (
             <div> 
-            <ScrollBox ref={(ref) => {this.scrollBox=ref}}/>
-            <button onClick={() => this.scrollBox.scrollToBottom()}>
-            맨 밑으로
-            </button>
-            <button onClick={() => this.scrollBox.scrollToTop()}>
-            맨 위로
-            </button>
+            <IterationSample />
             </div>
         );
     }
