f5922b5480d563a5819777d2d2483c091e2fa1fc
JANGHYUNn <kimjh2369@gmail.com>
2018-10-09 21:39:36 +0900

    LifeCycle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            color: '#000000'
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor()
+        });
+    }
+
     render() {
         return (
             <div> 
-            <IterationSample />
+                <button onClick={this.handleClick}>랜덤 색상</button>
+                <LifeCycleSample color={this.state.color}/>
             </div>
         );
     }
